34d2ee84fc821e873452e1942746a12fb3009587
Eric Toader <eric.toaderstefan@gmail.com>
2023-09-11 14:53:59 +0300

    Initial Commit

---
+++ Showtime/ShowtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowtimeApp.swift
+//  Showtime
+//
+//  Created by Eric Toader on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
